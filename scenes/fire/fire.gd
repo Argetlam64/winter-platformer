b2c8922e5aba35289de08a4c62ee7da473112337
Maj <majverhovnikmarovsek@gmail.com>
2025-12-15 18:31:41 +0100

    Fixed bug that crashed the game on lighting fire

--- scenes/fire/fire.gd
+++ scenes/fire/fire.gd
@@ -22,7 +22,7 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if Global.wood_count >= Global.required_wood and Input.is_action_just_pressed("interact") and available:
-		$Start.start()
+		$Start.play()
 		$Sprite2D.play("lit")
 		emit_signal("fire_lit")
 
